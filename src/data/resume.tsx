789901a16aba0007bdaa7c1ee6c2503348b379c1
Amol Yadav <amyssnipet@yahoo.com>
2024-08-29 12:10:53 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,14 +1,14 @@
 import { Icons } from "@/components/icons";
-import { HomeIcon, NotebookIcon } from "lucide-react";
+import { HomeIcon, NotebookIcon, CodeIcon } from "lucide-react";
 
 export const DATA = {
   name: "Dillion Verma",
   initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  url: "https://amyssnippet.vercel.app",
+  location: "Mumbai, MH",
+  locationLink: "https://www.google.com/maps/place/mumbai",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer. I love building things and helping people. Very active on Github.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
@@ -18,45 +18,46 @@ export const DATA = {
     "Typescript",
     "Node.js",
     "Python",
-    "Go",
-    "Postgres",
-    "Docker",
-    "Kubernetes",
-    "Java",
-    "C++",
+    "Git",
+    "MongoDB",
+    "Express",
+    "REST API",
+    "Angular",
+    "Fast Learner",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    { href: "/projects", icon: "CodeIcon", label: "Projects" },
   ],
   contact: {
-    email: "hello@example.com",
-    tel: "+123456789",
+    email: "amyssnipet@yahoo.com",
+    tel: "+917699900211",
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/amyssnippet",
         icon: Icons.github,
 
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://linkedin.com/in/yadav-amol",
         icon: Icons.linkedin,
 
         navbar: true,
       },
-      X: {
-        name: "X",
-        url: "https://dub.sh/dillion-twitter",
+      Whatsapp: {
+        name: "Whatsapp",
+        url: "https://wa.me/+917699900211",
         icon: Icons.x,
 
-        navbar: true,
+        navbar: false,
       },
       Youtube: {
         name: "Youtube",
-        url: "https://dub.sh/dillion-youtube",
+        url: "https://youtube.com/@amycode",
         icon: Icons.youtube,
         navbar: true,
       },
@@ -146,36 +147,36 @@ export const DATA = {
   ],
   education: [
     {
-      school: "Buildspace",
-      href: "https://buildspace.so",
-      degree: "s3, s4, sf1, s5",
+      school: "Atharva College of Engineering",
+      href: "https://atharvacoe.ac.in",
+      degree: "Bachelore Degree of Engineering in Information Technology",
       logoUrl: "/buildspace.jpg",
-      start: "2023",
-      end: "2024",
+      start: "2024",
+      end: "2028",
     },
     {
-      school: "University of Waterloo",
-      href: "https://uwaterloo.ca",
-      degree: "Bachelor's Degree of Computer Science (BCS)",
+      school: "SVP Junior College of Science",
+      href: "https://svpjc.in",
+      degree: "Grade 11 & 12",
       logoUrl: "/waterloo.png",
-      start: "2016",
+      start: "2023",
       end: "2021",
     },
     {
-      school: "Wilfrid Laurier University",
-      href: "https://wlu.ca",
-      degree: "Bachelor's Degree of Business Administration (BBA)",
+      school: "Blossomms High School",
+      href: "https://blossommshighschool.in/",
+      degree: "Grade 3 - 10",
       logoUrl: "/laurier.png",
-      start: "2016",
-      end: "2021",
+      start: "2021",
+      end: "2013",
     },
     {
-      school: "International Baccalaureate",
-      href: "https://ibo.org",
-      degree: "IB Diploma",
+      school: "KES International School",
+      href: "https://kesinternationalschool.org",
+      degree: "K - Grade 2",
       logoUrl: "/ib.png",
-      start: "2012",
-      end: "2016",
+      start: "2008",
+      end: "2013",
     },
   ],
   projects: [
